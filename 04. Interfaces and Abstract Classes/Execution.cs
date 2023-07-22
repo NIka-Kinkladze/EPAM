37294909bec2382624357f7ce5fb3569f3d10960
Nika Kinkladze <65700920+kinkla@users.noreply.github.com>
2023-07-22 15:16:04 +0400

    First stage

---
+++ 04. Interfaces and Abstract Classes/Execution.cs
@@ -0,0 +1,29 @@
+﻿using Flyers;
+using InterfacesAndStructures;
+
+namespace ExecutionMethod
+{
+    internal class Execution
+    {
+        public static void exetution()
+        {
+            Coordinate initialPosition = new Coordinate(0, 0, 0);
+            Coordinate destination = new Coordinate(10, 10, 10);
+
+            Bird bird = new Bird(initialPosition);
+            Airplane airplane = new Airplane(initialPosition);
+            Drone drone = new Drone(initialPosition);
+
+            Console.Write("Bird:");
+            Console.WriteLine($"Fly time to destination: {bird.GetFlyTime(destination)} hours");
+
+            Console.Write("\nAirplane:");
+            Console.WriteLine($"Fly time to destination: {airplane.GetFlyTime(destination)} hours");
+
+            Console.Write("\nDrone:");
+            Console.WriteLine($"Fly time to destination: {drone.GetFlyTime(destination)} hours");
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ 04. Interfaces and Abstract Classes/IFlyable.cs
@@ -0,0 +1,22 @@
+﻿namespace InterfacesAndStructures
+{
+    public struct Coordinate
+    {
+        public double X;
+        public double Y;
+        public double Z;
+
+        public Coordinate(double x, double y, double z)
+        {
+            this.X = x;
+            this.Y = y;
+            this.Z = z;
+        }
+    }
+
+    public interface IFlyable
+    {
+        void FlyTo(Coordinate newPoint);
+        double GetFlyTime(Coordinate newPoint);
+    }
+}
