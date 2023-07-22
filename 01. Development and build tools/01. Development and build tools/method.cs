dc11163a95466337ee77276e4408fcf6341ebd9a
Nika Kinkladze <65700920+kinkla@users.noreply.github.com>
2023-07-22 16:11:24 +0400

    added first Task

---
+++ 01. Development and build tools/01. Development and build tools/method.cs
@@ -0,0 +1,36 @@
+﻿public class Method
+{
+    static int GetMaxUnequalConsecutiveCharacters(string sequence)
+    {
+        if (string.IsNullOrEmpty(sequence))
+            return 0;
+
+        int maxConsecutive = 1;
+        int currentConsecutive = 1;
+
+        for (int i = 1; i < sequence.Length; i++)
+        {
+            if (sequence[i] != sequence[i - 1])
+            {
+                currentConsecutive++;
+            }
+            else
+            {
+                currentConsecutive = 1;
+            }
+
+            maxConsecutive = Math.Max(maxConsecutive, currentConsecutive);
+        }
+
+        return maxConsecutive;
+    }
+
+    public static void execution()
+    {
+        Console.WriteLine("Enter a sequence of symbols:");
+        string sequence = Console.ReadLine();
+
+        int maxConsecutive = GetMaxUnequalConsecutiveCharacters(sequence);
+        Console.WriteLine($"Maximum unequal consecutive characters: {maxConsecutive}");
+    }
+}
