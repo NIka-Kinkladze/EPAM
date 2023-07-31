c0c478d4986f4240396ca28f16ec6271072849e7
Nika Kinkladze <65700920+kinkla@users.noreply.github.com>
2023-07-31 16:05:05 +0400

    Small changes

---
+++ EPAM/EPAM/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace program
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Method.execution(); // Start the program execution by calling the execution method in the Method class
+        }
+    }
+}
